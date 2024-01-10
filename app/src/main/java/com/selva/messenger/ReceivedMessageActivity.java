455579258524ff7ff5160f107153e36160b7188d
Nuriya Akhmedshina <nyriya36@gmail.com>
2024-01-10 08:29:15 +0500

    ReceivedMessageActivity added

--- app/src/main/java/com/selva/messenger/ReceivedMessageActivity.java
+++ app/src/main/java/com/selva/messenger/ReceivedMessageActivity.java
@@ -3,16 +3,12 @@ package com.selva.messenger;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 
-public class CreateMessageActivity extends AppCompatActivity {
+public class ReceivedMessageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_message);
-    }
-
-    public void onClickSendMessage(View view) {
+        setContentView(R.layout.activity_received_message);
     }
 }
